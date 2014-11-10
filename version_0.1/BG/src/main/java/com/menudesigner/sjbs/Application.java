83516096cb29d4cb22cabc6c560e17ee182f8b6b
JIN Benli <benli0822@gmail.com>
2014-11-11 00:12:56 +0100

    Trying to configure for tiles 3

--- version_0.1/BG/src/main/java/com/menudesigner/sjbs/Application.java
+++ version_0.1/BG/src/main/java/com/menudesigner/sjbs/Application.java
@@ -2,26 +2,24 @@ package com.menudesigner.sjbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by JIN Benli on 27/10/14.
  */
 @Configuration
+@EnableAutoConfiguration
 @EnableJpaRepositories
 @Import(RepositoryRestMvcConfiguration.class)
-@EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-        context.close();
-
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
     }
 }
