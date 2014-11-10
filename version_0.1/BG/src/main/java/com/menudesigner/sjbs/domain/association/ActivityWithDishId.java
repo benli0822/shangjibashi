3a5f026a5eff4d10fbfd81c8c52cfe74fe2feeac
JIN Benli <benli0822@gmail.com>
2014-11-10 10:11:58 +0100

    begin to implements database service, end of road for mock factory test

---
+++ version_0.1/BG/src/main/java/com/menudesigner/sjbs/domain/association/ActivityWithDishId.java
@@ -0,0 +1,7 @@
+package com.menudesigner.sjbs.domain.association;
+
+/**
+ * Created by JIN Benli on 08/11/14.
+ */
+public class ActivityWithDishId {
+}
---
+++ version_0.1/BG/src/test/java/com/menudesigner/sjbs/domain/MockFactoryTest.java
@@ -0,0 +1,7 @@
+package com.menudesigner.sjbs.domain;
+
+/**
+ * Created by JIN Benli on 10/11/14.
+ */
+public class MockFactoryTest {
+}
