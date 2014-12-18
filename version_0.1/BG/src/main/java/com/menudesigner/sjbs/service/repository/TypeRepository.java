a6db1393b39316916cd4aa4998867697e7002773
JIN Benli <benli0822@gmail.com>
2014-12-18 11:02:02 +0100

    create simple type test

---
+++ version_0.1/BG/src/main/java/com/menudesigner/sjbs/service/repository/TypeRepository.java
@@ -0,0 +1,11 @@
+package com.menudesigner.sjbs.service.repository;
+
+import com.menudesigner.sjbs.domain.Type;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by JIN Benli on 18/12/14.
+ */
+public interface TypeRepository extends CrudRepository<Type, Long> {
+
+}
