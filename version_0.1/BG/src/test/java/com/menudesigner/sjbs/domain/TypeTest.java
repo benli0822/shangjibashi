1d7ec853f1c573ddc50311089a7e0e1f0e14ae59
JIN Benli <benli0822@gmail.com>
2014-12-20 20:14:26 +0100

    added option, type with dish association tests

--- version_0.1/BG/src/test/java/com/menudesigner/sjbs/domain/TypeTest.java
+++ version_0.1/BG/src/test/java/com/menudesigner/sjbs/domain/TypeTest.java
@@ -1,6 +1,7 @@
 package com.menudesigner.sjbs.domain;
 
 import com.menudesigner.sjbs.Application;
+import com.menudesigner.sjbs.service.repository.DishRepository;
 import com.menudesigner.sjbs.service.repository.TypeRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.sql.Time;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
@@ -30,6 +34,9 @@ public class TypeTest {
 
     @Autowired
     private TypeRepository typeRepository;
+
+    @Autowired
+    private DishRepository dishRepository;
 
     @Test
     public void addSimpleTypeTest() {
@@ -64,5 +71,40 @@ public class TypeTest {
 
         assertThat(theType, notNullValue());
         assertThat(theRes, nullValue());
+    }
+
+    @Test
+    public void addTypeWithDishTest() {
+        Type type = new Type();
+
+        type.setIs_firstmenu(true);
+        type.setIs_secondmenu(false);
+        type.setName("cold");
+        type.setDescription("cold");
+        type.setIs_for_customize(false);
+
+        Dish newDish = new Dish();
+        newDish.setName("coca");
+        newDish.setIs_typed(false);
+        newDish.setImg_path("abc");
+        newDish.setPrice(5);
+        newDish.setDescription("abc");
+        newDish.setDisabled(false);
+        newDish.setStart_time(new Time(10,10,10));
+        newDish.setEnd_time(new Time(10,11,10));
+        newDish.setStart_date(new Date(2014,10,12));
+        newDish.setEnd_date(new Date(2014,11,12));
+
+        newDish.addType(type);
+        type.addDish(newDish);
+
+        Dish res1 = dishRepository.save(newDish);
+        Type res2 = typeRepository.save(type);
+
+        assertThat(res1, notNullValue());
+        assertThat(res2, notNullValue());
+
+        assertThat(res1.getTypes().contains(res2), is(Boolean.TRUE));
+        assertThat(res2.getDishes().contains(res1), is(Boolean.TRUE));
     }
 }
--- version_0.1/BG/src/test/java/com/menudesigner/sjbs/service/UserRepositoryTest.java
+++ version_0.1/BG/src/test/java/com/menudesigner/sjbs/service/UserRepositoryTest.java
@@ -1,41 +1,41 @@
-package com.menudesigner.sjbs.service;
-
-import com.menudesigner.sjbs.Application;
-import com.menudesigner.sjbs.domain.User;
-import com.menudesigner.sjbs.service.repository.UserRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.util.List;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
-/**
-* Created by JIN Benli on 12/10/14.
-*/
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-public class UserRepositoryTest {
-
-    @Autowired
-    private UserRepository repository;
-
-    @Test
-    public void searchNameNotExist() {
-        List<User> notFound = repository.findUserByUsername("Not Found");
-        assertThat(notFound.size(), is(0));
-    }
-
-    @Test
-    public void searchRoot() {
-        List<User> notFound = repository.findUserByUsername("root");
-        assertThat(notFound.size(), is(1));
-    }
-
-
-}
+//package com.menudesigner.sjbs.service;
+//
+//import com.menudesigner.sjbs.Application;
+//import com.menudesigner.sjbs.domain.User;
+//import com.menudesigner.sjbs.service.repository.UserRepository;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import java.util.List;
+//
+//import static org.hamcrest.Matchers.is;
+//import static org.junit.Assert.assertThat;
+//
+///**
+//* Created by JIN Benli on 12/10/14.
+//*/
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = Application.class)
+//public class UserRepositoryTest {
+//
+//    @Autowired
+//    private UserRepository repository;
+//
+//    @Test
+//    public void searchNameNotExist() {
+//        List<User> notFound = repository.findUserByUsername("Not Found");
+//        assertThat(notFound.size(), is(0));
+//    }
+//
+//    @Test
+//    public void searchRoot() {
+//        List<User> notFound = repository.findUserByUsername("root");
+//        assertThat(notFound.size(), is(1));
+//    }
+//
+//
+//}
