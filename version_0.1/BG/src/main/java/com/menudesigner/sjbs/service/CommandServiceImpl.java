71425b9f2845737bb1f7b599651d01dd3b6f8e27
JIN Benli <benli0822@gmail.com>
2014-12-23 15:20:18 +0100

    add dish to command or update dish in command, tested

--- version_0.1/BG/src/main/java/com/menudesigner/sjbs/service/CommandServiceImpl.java
+++ version_0.1/BG/src/main/java/com/menudesigner/sjbs/service/CommandServiceImpl.java
@@ -1,11 +1,9 @@
 package com.menudesigner.sjbs.service;
 
 import com.menudesigner.sjbs.domain.Command;
+import com.menudesigner.sjbs.domain.CommandDish;
 import com.menudesigner.sjbs.domain.Dish;
-import com.menudesigner.sjbs.service.repository.ActivityRepository;
-import com.menudesigner.sjbs.service.repository.CommandRepository;
-import com.menudesigner.sjbs.service.repository.DishRepository;
-import com.menudesigner.sjbs.service.repository.MenuRepository;
+import com.menudesigner.sjbs.service.repository.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,14 +28,16 @@ public class CommandServiceImpl implements CommandService {
     private final DishRepository dishRepository;
     private final ActivityRepository activityRepository;
     private final MenuRepository menuRepository;
+    private final CommandDishRepository commandDishRepository;
 
     @Autowired
-    public CommandServiceImpl(JdbcTemplate jdbcTemplate, CommandRepository commandRepository, DishRepository dishRepository, ActivityRepository activityRepository, MenuRepository menuRepository) {
+    public CommandServiceImpl(JdbcTemplate jdbcTemplate, CommandRepository commandRepository, DishRepository dishRepository, ActivityRepository activityRepository, MenuRepository menuRepository, CommandDishRepository commandDishRepository) {
         this.jdbcTemplate = jdbcTemplate;
         this.commandRepository = commandRepository;
         this.dishRepository = dishRepository;
         this.activityRepository = activityRepository;
         this.menuRepository = menuRepository;
+        this.commandDishRepository = commandDishRepository;
     }
 
     @Override
@@ -79,17 +79,20 @@ public class CommandServiceImpl implements CommandService {
         logger.debug("Dish " + theDish.toString() + " Command " + theCommand.toString() + " with quantity: " + quantity);
         //TODO how to handle quantity problem?
 
-        if(!theDish.getCommands().contains(theCommand) && !theCommand.getDishes().contains(theDish)) {
-//            theDish.addCommand(theCommand);
-//            theCommand.addDish(theDish);
-//            Dish res1 = dishRepository.save(theDish);
-//            Command res2 = commandRepository.save(theCommand);
+        // first check the association
+        List<CommandDish> associations = commandDishRepository.findCommandDishByCommandAndDish(theCommand, theDish);
 
-//            String SQL = "UPDATE jb_menu_design.md_command_dish SET quantity = ? WHERE command_id = ? AND dish_id = ?";
-//            jdbcTemplate.update(SQL, quantity, theCommand.getId(), theDish.getId());
+        if(associations.size() == 0) {
+            // if neither command nor dish has been added to each other, the add the association
+            theCommand.addDish(theDish, quantity);
+
+            return true;
+        } else if(associations.size() == 1) {
+            associations.get(0).setQuantity(quantity);
+
             return true;
         } else {
-            logger.error("neither dish " + theDish.toString() + "nor command " + theCommand.toString() + "has already associated with each other!");
+            logger.error("neither dish " + theDish.toString() + "nor command " + theCommand.toString() + "has already associated with each other more than one time!");
             return false;
         }
     }
--- version_0.1/BG/src/test/java/com/menudesigner/sjbs/service/CommandServiceTest.java
+++ version_0.1/BG/src/test/java/com/menudesigner/sjbs/service/CommandServiceTest.java
@@ -1,86 +1,78 @@
-//package com.menudesigner.sjbs.service;
-//
-//import com.menudesigner.sjbs.Application;
-//import com.menudesigner.sjbs.domain.Command;
-//import com.menudesigner.sjbs.domain.Dish;
-//import com.menudesigner.sjbs.service.repository.CommandRepository;
-//import com.menudesigner.sjbs.service.repository.DishRepository;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.transaction.TransactionConfiguration;
-//
-//import javax.transaction.Transactional;
-//import java.sql.Date;
-//import java.sql.Time;
-//import java.util.List;
-//
-//import static org.hamcrest.Matchers.is;
-//import static org.hamcrest.Matchers.notNullValue;
-//import static org.junit.Assert.assertThat;
-//import static org.junit.Assert.assertTrue;
-//
-///**
-// * Created by JIN Benli on 22/12/14.
-// */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ActiveProfiles(value = "test")
-//@Transactional
-//@TransactionConfiguration(defaultRollback = true)
-//@SpringApplicationConfiguration(classes = Application.class)
-//public class CommandServiceTest {
-//    private static final Logger logger = LoggerFactory.getLogger(CommandServiceTest.class);
-//
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
-//    @Autowired
-//    private CommandRepository commandRepository;
-//    @Autowired
-//    private DishRepository dishRepository;
-//    @Autowired
-//    private CommandService commandService;
-//
-//    @Test
-//    public void addDishToCommandTest() {
-//        Command command = new Command();
-//
-//        command.setTitle("Bob");
-//        command.setMsg_extra("test");
-//        command.setOrder_date(new Date(2014, 10 ,12));
-//        command.setOrder_time(new Time(20, 20, 20));
-//        command.setTable_no(1);
-//        command.setClient_no(5);
-//
-//        Dish newDish = new Dish();
-//        newDish.setName("coca");
-//        newDish.setIs_typed(false);
-//        newDish.setImg_path("abc");
-//        newDish.setPrice(5);
-//        newDish.setDescription("abc");
-//        newDish.setDisabled(false);
-//        newDish.setStart_time(new Time(10,10,10));
-//        newDish.setEnd_time(new Time(10,11,10));
-//        newDish.setStart_date(new Date(2014,10,12));
-//        newDish.setEnd_date(new Date(2014,11,12));
-//
-//        Dish theDish = dishRepository.save(newDish);
-//        Command theCommand = commandRepository.save(command);
-//
-//        boolean res = commandService.addDishToCommand(theDish.getId(), theCommand.getId(), 2);
-//
-//        String SQL = "SELECT quantity FROM jb_menu_design.md_command_dish WHERE command_id = ? AND dish_id = ?";
-//        List<Integer> quantityList = jdbcTemplate.queryForList(SQL, Integer.class, theCommand.getId(), theDish.getId());
-//
-//        assertThat(theDish, notNullValue());
-//        assertThat(theCommand, notNullValue());
-//        assertTrue(res);
-//        assertThat(quantityList.size(), is(0));
-//        assertThat(quantityList.get(0) == 2, is(Boolean.TRUE));
-//    }
-//}
+package com.menudesigner.sjbs.service;
+
+import com.menudesigner.sjbs.Application;
+import com.menudesigner.sjbs.domain.CommandDish;
+import com.menudesigner.sjbs.domain.Dish;
+import com.menudesigner.sjbs.service.repository.CommandDishRepository;
+import com.menudesigner.sjbs.service.repository.CommandRepository;
+import com.menudesigner.sjbs.service.repository.DishRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import javax.transaction.Transactional;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by JIN Benli on 22/12/14.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles(value = "test")
+@Transactional
+@TransactionConfiguration(defaultRollback = true)
+@SpringApplicationConfiguration(classes = Application.class)
+public class CommandServiceTest {
+    private static final Logger logger = LoggerFactory.getLogger(CommandServiceTest.class);
+
+    @Autowired
+    private CommandRepository commandRepository;
+    @Autowired
+    private DishRepository dishRepository;
+    @Autowired
+    private CommandService commandService;
+    @Autowired
+    private CommandDishRepository commandDishRepository;
+
+    @Test
+    public void addDishToCommandTest() {
+
+        // TODO need to covered by dish service
+        Dish newDish = new Dish();
+        newDish.setName("coca");
+        newDish.setIs_typed(false);
+        newDish.setImg_path("abc");
+        newDish.setPrice(5);
+        newDish.setDescription("abc");
+        newDish.setDisabled(false);
+        newDish.setStart_time(new Time(10, 10, 10));
+        newDish.setEnd_time(new Time(10, 11, 10));
+        newDish.setStart_date(new Date(2014, 10, 12));
+        newDish.setEnd_date(new Date(2014, 11, 12));
+
+        // TODO like this
+        Dish theDish = dishRepository.save(newDish);
+        long command_id = commandService.addCommand("Bob", "test", 1, 5);
+
+        boolean res = commandService.addDishToCommand(theDish.getId(), command_id, 2);
+
+        List<CommandDish> commandDishs = commandDishRepository.findCommandDishByCommandAndDish(commandRepository.findOne(command_id), theDish);
+
+        assertThat(theDish, notNullValue());
+        assertThat(command_id, not(-1L));
+        assertTrue(res);
+        assertThat(commandDishs.size(), is(1));
+        assertThat(commandDishs.get(0).getQuantity() == 2, is(Boolean.TRUE));
+    }
+}
