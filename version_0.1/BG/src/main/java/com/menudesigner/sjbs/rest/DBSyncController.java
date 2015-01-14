06590fcb6c819a820663db0aaee363ee5bcff0c0
JIN Benli <benli0822@gmail.com>
2015-01-14 15:24:12 +0100

    rest service fot getting database sync file

---
+++ version_0.1/BG/src/main/java/com/menudesigner/sjbs/rest/DBSyncController.java
@@ -0,0 +1,64 @@
+package com.menudesigner.sjbs.rest;
+
+import com.menudesigner.sjbs.service.connection.SQLiteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ * Created by JIN Benli on 14/01/15.
+ */
+@Controller
+@RequestMapping("/api/sync")
+public class DBSyncController {
+    private static final Logger logger = LoggerFactory.getLogger(DBSyncController.class);
+
+    @Autowired
+    private SQLiteService sqLiteService;
+
+    @RequestMapping(value = "/getDbFile", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    FileSystemResource getDbFile(HttpServletRequest request,
+                                 HttpServletResponse response) throws URISyntaxException {
+        URL url = this.getClass().getResource("/shell/database.sqlite");
+
+        sqLiteService.generateDB();
+
+        File file = new File(url.toURI());
+
+        ServletContext context = request.getServletContext();
+
+        // get MIME type of the file
+        String mimeType = context.getMimeType(url.getPath());
+        if (mimeType == null) {
+            // set to binary type if MIME mapping not found
+            mimeType = "application/octet-stream";
+        }
+        logger.info("MIME type: " + mimeType);
+
+        // set content attributes for the response
+        response.setContentType(mimeType);
+        response.setContentLength((int) file.length());
+
+        // set headers for the response
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"",
+                file.getName());
+        response.setHeader(headerKey, headerValue);
+
+        return new FileSystemResource(url.getFile());
+    }
+}
