0a1de5c5292fd22e343406ddcaf4bf18aabcea88
IBM2 <ibm2@MacBook-Pro-de-IBM2.local>
2015-03-17 16:59:48 +0100

    create home tab view

---
+++ version_iphone/Diancai/Diancai/ViewController/Home/MDHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MDHomeViewController.h
+//  Diancai
+//
+//  Created by IBM2 on 17/03/2015.
+//  Copyright (c) 2015 Xiaojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDHomeViewController : UIViewController
+
+@end
---
+++ version_iphone/Diancai/Diancai/ViewController/Home/MDHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  MDHomeViewController.m
+//  Diancai
+//
+//  Created by IBM2 on 17/03/2015.
+//  Copyright (c) 2015 Xiaojun. All rights reserved.
+//
+
+#import "MDHomeViewController.h"
+
+@interface MDHomeViewController ()
+
+@end
+
+@implementation MDHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
